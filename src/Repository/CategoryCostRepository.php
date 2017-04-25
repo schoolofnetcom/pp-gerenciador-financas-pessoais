87387a9d2c7080321a1f2a80e442b6e393f2a0e4
Luiz Carlos <argentinaluiz@gmail.com>
2017-04-25 11:42:12 -0300

    Update CategoryCostRepository.php

--- src/Repository/CategoryCostRepository.php
+++ src/Repository/CategoryCostRepository.php
@@ -28,7 +28,6 @@ class CategoryCostRepository extends DefaultRepository implements CategoryCostRe
             ->whereBetween('date_launch', [$dateStart, $dateEnd])
             ->where('category_costs.user_id', $userId)
             ->whereNotNull('bill_pays.category_cost_id')
-            ->groupBy('value')
             ->groupBy('category_costs.name')
             ->get();
         return $categories->toArray();
